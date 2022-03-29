5cbc451a90e04f107080bd9b99050b08ebb2b563
Artur Petrov <artur.petrov@auto.ria.com>
2022-03-29 09:07:38 +0300

    Delete trash :^>

--- app/helpers/delay.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // eslint-disable-next-line no-promise-executor-return
-  delay: (ms) => new Promise((resolve) => setTimeout(resolve, ms)),
-};
--- app/helpers/users.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-  getUserName(ctx) {
-    return ctx.update.message.from?.username
-      ? `@${ctx.update.message.from?.username}`
-      : ctx.update.message.from?.first_name;
-  },
-};
