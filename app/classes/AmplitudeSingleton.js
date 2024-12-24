52b9b8024ef06a6646d2e974442c427b9c8ae49f
arturpetrov <petrov0397@gmail.com>
2024-12-25 01:02:32 +0200

    Fix AmplitudeSingleton.js

--- app/classes/AmplitudeSingleton.js
+++ app/classes/AmplitudeSingleton.js
@@ -1,9 +1,12 @@
 const config = require('config');
 
-const Amplitude = '@amplitude/node';
+// eslint-disable-next-line import/no-extraneous-dependencies
+const Amplitude = require('@amplitude/node');
 
 const apiKey = config.analytics.Amplitude.key;
 
-const client = Amplitude.init(apiKey);
+const client = Amplitude.init(apiKey, {
+  retryClass: new Amplitude.OfflineRetryHandler(apiKey),
+});
 
 module.exports = client;
